ffa9909ca6ad4ef24aba50c730f600e191108bf4
andy <chartreusesky@pm.me>
2025-09-08 14:19:49 -0400

    added gwf, responsive pass
    Added google web fonts and polished some responsive elements

--- angular/src/app/shared/post-viewers/post-viewer.ts
+++ angular/src/app/shared/post-viewers/post-viewer.ts
@@ -8,11 +8,11 @@ import { post } from '../../models/post-model';
   selector: 'app-postViewer',
   imports: [CommentViewer],
   template: `
-    <div class="card mb-4 p-5" style="max-width: 1024px; filter: drop-shadow(0px 0px 15px #dadadaff);">
+    <div class="card mb-4 p-sm-2 p-md-5 mx-auto" style="max-width: 1024px; filter: drop-shadow(0px 0px 15px #dadadaff);">
         <div class="card-body">
             <h1 [innerHTML]="post!.title.rendered"></h1>
             <blockquote class="blockquote float-right mb-0">
-              <section class="blockquote-footer my-4">
+              <section class="blockquote-footer text-center my-4">
                 @if (this.identity.authorAvatar(post)) {
                   <img [src]="this.identity.authorAvatar(post)" alt="Author avatar" class="rounded-circle" width="48" height="48">
                 }
