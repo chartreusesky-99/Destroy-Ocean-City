564800b28a0c1c558fd6b03f15cca958e490e090
andy <chartreusesky@pm.me>
2026-05-05 09:42:33 -0400

    updated privacy policy and added aria elements

--- angular/src/app/structural/header/header.ts
+++ angular/src/app/structural/header/header.ts
@@ -1,17 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NgClass } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ThemeService } from '../../services/theme-service';
 
 @Component({
   selector: 'doc-header',
-  imports: [ NgClass, RouterLink ],
+  imports: [ NgClass, RouterLink, RouterLinkActive ],
   templateUrl: './header.html'
 })
 export class Header {
 
+  private router = inject(Router);
+
   constructor( public theme: ThemeService ) {}
 
   ngOnInit() {}
+
+  isBlogActive(): boolean {
+    const url = this.router.url;
+    return url.startsWith('/blog') || url.startsWith('/content/');
+  }
+
+  isMerchActive(): boolean {
+    const url = this.router.url;
+    return url.startsWith('/merch') || url.startsWith('/merchandise/');
+  }
 
 }
